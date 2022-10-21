d85d5b7e290689ffbd640b7570e2b53c8f6af18a
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-10-21 15:40:34 +0500

    add commands

---
+++ Homework_8/data_comm.py
@@ -0,0 +1,46 @@
+import os
+import menu
+import creat_base as cb
+import add_key as ak
+import add_user as us
+import print_user as pu
+
+def clear(): return os.system('cls')
+
+def data_comm():
+    exit = True
+    data_flag = False
+    data = cb.read_file("data_base.json")
+    if data == []:
+        data_flag = True
+    while exit:
+        if data_flag:
+            what = 3
+            data_flag = False
+        else:
+            what = menu.menu()
+        if what == 1:
+            clear()
+            pu.get_print_user_for_one(data)
+            cb.write_file(data)
+            clear()
+        elif what == 2:
+            clear()
+            pu.get_print_user_all(data)
+            clear()
+        elif what == 3:
+            clear()
+            us.get_add_user(data)
+            cb.write_file(data)
+            clear()
+        elif what == 4:
+            clear()
+            ak.get_add_key(data)
+            cb.write_file(data)
+            clear()
+        else:
+            cb.write_file(data, "data_base_2.json")
+            exit = False
+
+
+data_comm()
