21b46c154b163546da89884eaad87c19ab6cf968
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-10-21 15:36:42 +0500

    read and write file

---
+++ Homework_8/creat_base.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+
+def read_file(file_name):
+    if os.path.exists(file_name):
+        with open(file_name, 'r', encoding='utf8') as read_file:
+            data = json.load(read_file)
+    else:
+        data = []
+        print('\n'
+              'Информационная система компании GRAPH\n'
+              'Введите данные первого сотрудника\n')
+    return data
+
+
+def write_file(data, file_reserve='data_base.json'):
+    with open(file_reserve, 'w', encoding='utf8') as write_file:
+        json.dump(data, write_file, ensure_ascii = False, indent = 4)
