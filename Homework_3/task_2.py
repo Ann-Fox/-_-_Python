248e828557c2dac30a9b0063647694956f166373
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-11-04 13:11:07 +0500

    added tasks seminar 6 (Svytoslav)

--- Homework_3/task_2.py
+++ Homework_3/task_2.py
@@ -6,22 +6,37 @@
 #  - [2, 3, 5, 6] => [12, 15]
 
 
-from random import randint
-import math
+def dbl(lst: list) -> list:
+    count_dbl = (len(lst)+1) // 2
+    res_list = [lst[i]*lst[-i-1] for i in range(count_dbl)]
+    return res_list
 
-def get_numbers(n, first, last):
-    return [randint(first, last) for i in range(n)]
 
-def mult_pairs(mylist):
-    return [mylist[i] * mylist[-i - 1] for i in range(math.ceil(len(mylist)/2))]
+list_1 = [[2, 3, 4, 5], [2, 3, 4, 5, 6]]
+print(list(map(dbl, list_1)))
 
-n = 7
-first = 1
-last = 10
 
-mylist = get_numbers(n, first, last)
-print(mylist)
-print(mult_pairs(mylist))
+
+
+
+# import math
+# from random import randint
+
+# def get_numbers(n, first, last):
+#     return [randint(first, last) for i in range(n)]
+
+
+# def mult_pairs(mylist):
+#     return [mylist[i] * mylist[-i - 1] for i in range(math.ceil(len(mylist)/2))]
+
+
+# n = 7
+# first = 1
+# last = 10
+
+# mylist = get_numbers(n, first, last)
+# print(mylist)
+# print(mult_pairs(mylist))
 
 
 # Второй способ:
@@ -30,9 +45,9 @@ print(mult_pairs(mylist))
 #     results = []
 #     while len(numbers) > 1:
 #         results.append(numbers[0]*numbers[-1])
-#         del numbers[0] 
-#         del numbers[-1] 
-#     if len(numbers) ==1: results.append(numbers[0]**2)       
+#         del numbers[0]
+#         del numbers[-1]
+#     if len(numbers) ==1: results.append(numbers[0]**2)
 #     return results
 
 # print(pairs_mult(mylist))
